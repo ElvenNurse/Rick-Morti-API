4046babe1e5d64577a81ac6b42f36057b2ce443a
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2020-03-26 13:11:39 +0200

    first commit

---
+++ src/main/java/com/example/rickmortiapi/RickMortiApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.rickmortiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickMortiApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickMortiApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rickmortiapi/RickMortiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rickmortiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RickMortiApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
